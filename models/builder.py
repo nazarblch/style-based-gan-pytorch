b419be2c4cf7a5311af91784500e4b2c266ae4ac
nazar <sss>
2019-11-15 20:43:21 +0300

    123

--- models/builder.py
+++ models/builder.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List, Dict, Callable
+from typing import Tuple, List, Dict, Callable, Set
 from torch import nn, Tensor
 import torch
 import itertools
@@ -71,8 +71,35 @@ class ModuleBuilder:
         self.nodes: Dict[str, NamedModule] = {}
         self.edges: List[Tuple[List[str], str]] = []
 
-    def sub_graph(self, input_nodes: List[str]):
-        return self
+    def find_children(self, nodes: Set[str]) -> Set[str]:
+        ch = set(map(lambda e: e[1], filter(lambda e: len(set(e[0]) & nodes) > 0, self.edges)))
+        return set(filter(lambda n: n not in nodes, ch))
+
+    def are_linked(self, n1: str, n2: str):
+
+        if n1 == n2:
+            return True
+
+        dep_edges = [e[0] for e in filter(lambda e: e[1] == name, self.edges)]
+
+
+    def sub_graph(self, input_nodes: List[str], output: str):
+        sub_graph = ModuleBuilder()
+
+        cur_nodes = set(input_nodes)
+        new_nodes = self.find_children(cur_nodes)
+
+        while len(new_nodes) > 0:
+            cur_nodes += new_nodes
+            new_nodes = self.find_children(cur_nodes)
+
+        nodes = dict([(node, self.nodes[node]) for node in cur_nodes])
+        edges = filter(lambda e: len(set(e[0]) & cur_nodes) > 0, self.edges)
+
+        sub_graph.nodes = nodes
+        sub_graph.edges = edges
+
+        return sub_graph
 
     def add_module(self, name: str, module: nn.Module, from_names: List[str], to_names: List[str]):
         self.nodes[name] = NamedModule(module, from_names, to_names)
@@ -103,12 +130,30 @@ class ModuleBuilder:
     def build(self, input_nodes: List[str], output_name: str) -> NamedModule:
         sub_builder = self.sub_graph(input_nodes)
 
-        def fwd(input: Dict[str, Tensor]):
+        def fwd(input: Dict[str, Tensor]) -> Dict[str, Tensor]:
             memory = OutputMemory()
 
             for name in input_nodes:
                 out = sub_builder.nodes[name].forward(input)
                 memory.add_output(name, out)
+
+            return sub_builder.compute(output_name, memory)
+
+        modules = nn.ModuleList(
+           map(lambda m: m.module, sub_builder.nodes.values())
+        )
+
+        from_names = list(set(itertools.chain(*[self.nodes[node].from_names for node in input_nodes])))
+        to_names = self.nodes[output_name].to_names
+
+        return NamedModuleObject(
+            modules,
+            from_names,
+            to_names,
+            fwd
+        )
+
+
 
 
 
